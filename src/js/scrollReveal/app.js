e3b3b5308a90d80ef3a6dfb0a96a4dcd880dde3e
Oskar <oskar.lr.sterner@gmail.com>
2023-03-28 22:19:14 +0200

    Mobile version ready

---
+++ src/js/scrollReveal/app.js
@@ -0,0 +1,44 @@
+ScrollReveal().reveal('#trade',{
+    delay: 50,
+    duration: 1500,
+    opacity: 0,
+    distance: "50%",
+    origin: "right",
+    reset: true
+});
+
+ScrollReveal().reveal('#guides',{
+    delay: 50,
+    duration: 1500,
+    opacity: 0,
+    distance: "50%",
+    origin: "left",
+    reset: true
+});
+
+ScrollReveal().reveal('#giveaway',{
+    delay: 50,
+    duration: 1500,
+    opacity: 0,
+    distance: "50%",
+    origin: "right",
+    reset: true
+});
+
+ScrollReveal().reveal('#join-dc',{
+    delay: 75,
+    duration: 1500,
+    opacity: 0,
+    distance: "80%",
+    origin: "top",
+    reset: true
+});
+
+ScrollReveal().reveal('#iframe-dc',{
+    delay: 75,
+    duration: 1500,
+    opacity: 0,
+    distance: "80%",
+    origin: "bottom",
+    reset: true
+});
